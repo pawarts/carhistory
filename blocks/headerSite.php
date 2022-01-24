71c6591bc8b9bf8e98ff91375885e28f5d981710
pawarts <98350667+pawarts@users.noreply.github.com>
2022-01-24 21:08:17 +0200

    Add files via upload

---
+++ blocks/headerSite.php
@@ -0,0 +1,13 @@
+<header class="header">
+    <img src="<?= $folder?>img/logo.svg" alt="" class="logo">
+    <nav class="navBtn">
+        <a href="../index.php" class="btn">Головна</a>
+        <a href="" class="btn">Про мене</a>
+        <a href="<?=$point?>history/history.php" class="btn">Історії</a>
+    </nav>
+    <div class="user">
+        <a href="">
+            <img src="<?= $folder?>img/user1.svg" alt="" class="userIcon">
+        </a>
+    </div>
+</header>
